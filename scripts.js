613668b04173dd25884b2099e53f7bffc4c1de8a
felipefbpa <felipe839517@hotmail.com>
2023-03-04 09:29:43 -0300

    Correção do icone up down e mudança do duv freq

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
-let changeIcon = function(icon) {
-   icon.classList.toggle('fa-angle-down')
-   icon.classList.toggle('fa-angle-up')
+var accordions = document.getElementsByClassName("accordion");
 
+for (var i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function() {
+    this.classList.toggle('is-open');
+
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      // accordion is currently open, so close it
+      content.style.maxHeight = null;
+    } else {
+      // accordion is currently closed, so open it
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
 }
